37e734e0e0a549edc1b6bdb01f62aca448ec1b5d
HyuuuRiie <wahyuseptiadifajri11@gmail.com>
2021-09-28 13:51:45 +0700

    finishing assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-
-	<h1>About ME</h1>
+<div class="container-fluid">
+	<h1 class="mt-3">About ME</h1>
+	<img src="<?= BASEURL;?> /img/profile.jpg" alt="Wahyu" width="200" class="rounded-circle shadow">
 	<p>Halo, Nama Saya <?= $data['nama']; ?>, Saya adalah seorang <?= $data['pekerjaan'] ?></p>
+</div>
